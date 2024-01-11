67d6981d3aa08b238bb4b4f607a49d2ec80f9375
ZussmanFranz <zhenya.pyryev@gmail.com>
2024-01-11 02:18:58 +0100

    Update Map.js

--- src/component/Map.js
+++ src/component/Map.js
@@ -2,17 +2,18 @@
 import React, { useEffect, useState } from "react";
 import { MapContainer, TileLayer, Marker, Popup, useMap } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
-import icon from "../images/user.jpg";
+import icon from "../images/icon.png";
 import L from "leaflet";
+import "leaflet-routing-machine/dist/leaflet-routing-machine"; 
+import "leaflet-routing-machine/dist/leaflet-routing-machine.css"; // Add this line for CSS
+import "leaflet-control-geocoder/dist/Control.Geocoder";
 import MevoParking from "../mevoParking";
-import TierParking from "../tierParking";
 
 export default function Map({ coords, display_name, placeName }) {
     const [pointsOfInterest, setPointsOfInterest] = useState([]);
-
+    const [showMapView, setShowMapView] = useState(false);
+    const [waypoints, setWaypoints] = useState([]);
     const { latitude, longitude } = coords;
-    // let lat = coords.latitude;
-    // let lng = coords.longitude;
 
     const customIcon = new L.Icon({
         iconUrl: icon,
@@ -38,6 +39,7 @@ export default function Map({ coords, display_name, placeName }) {
                     let nodeId = node.getAttribute('id');
                     let latitude = node.getAttribute('lat');
                     let longitude = node.getAttribute('lon');
+                    console.log(node)
                     nodesInfo.push([latitude, longitude]);
                 }
                 setPointsOfInterest(nodesInfo);
@@ -51,13 +53,65 @@ export default function Map({ coords, display_name, placeName }) {
             getPointsOfInterest();
         }
     }, [latitude, longitude, placeName]);
+
+    function getPosition() {
+        return new Promise((resolve, reject) => {
+            navigator.geolocation.getCurrentPosition(resolve, reject);
+        });
+    }
+
+    const handleMarkerClick = async (markerLat, markerLng) => {
+        try {
+            if (waypoints.length === 0) {
+                let position = await getPosition();
+                let userCoordinates = [position.coords.latitude, position.coords.longitude];
+    
+                if (userCoordinates) {
+                    setWaypoints([userCoordinates]);
+                    console.log("User's Coordinates appended:", userCoordinates);
+                } else {
+                    console.error("User's coordinates are not available.");
+                }
+            }
+    
+            var Point = [markerLat, markerLng];
+            if (Point) {
+                setWaypoints(prevWaypoints => [...prevWaypoints, Point]);
+                console.log("Point's Coordinates appended:", Point);
+            } else {
+                console.error("Point's coordinates are not available.");
+            }
+        } catch (error) {
+            console.error('Error adding waypoint:', error);
+        }
+    };
+
     
 
     function MapView() {
         const map = useMap();
+
+        const waypointsArray = waypoints.map(coords => L.latLng(coords[0], coords[1]));
+
+        L.Routing.control({
+            waypoints: waypointsArray,
+            routeWhileDragging: true,
+            geocoder: L.Control.Geocoder.nominatim()
+        }).addTo(map);
+
+        console.log("Number of points: ", waypoints.length);
+
         map.setView([latitude, longitude], map.getZoom());
+        setShowMapView(false); // Hide the MapView after rendering
+
         return null;
     }
+
+    useEffect(() => {
+        if (waypoints.length > 0) {
+            setShowMapView(true); // Show the MapView component when waypoints are present
+        }
+    }, [waypoints]);
 
     return (
         <MapContainer
@@ -66,7 +120,6 @@ export default function Map({ coords, display_name, placeName }) {
             zoom={5}
             scrollWheelZoom={true}
         >
-
             <TileLayer
                 attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
                 url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
@@ -80,9 +133,14 @@ export default function Map({ coords, display_name, placeName }) {
                     <Popup>You are GAY</Popup>
                 </Marker>
             ))}
+            
             <MevoParking />
-            <TierParking /> 
             <MapView />
         </MapContainer>
     );
-}
+    /*
+    {
+                console.log(MevoParking())
+
+            }*/
+        }
